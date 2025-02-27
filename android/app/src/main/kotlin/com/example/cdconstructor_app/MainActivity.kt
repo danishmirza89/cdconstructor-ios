b83a32163a6a89b54a2b5cc271b5bf55a07ed1a1
Mirza DanishBaig <danishmrza2000@gmail.com>
2025-02-27 19:32:12 +0500

    Initial cdconstructor app commit

---
+++ android/app/src/main/kotlin/com/example/cdconstructor_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cdconstructor_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CD Constructor',
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MyHomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: 200,
+          height: 200,
+          child: Image.asset('assets/logo.png', fit: BoxFit.contain),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late WebViewController _controller;
+
+  Future<void> _launchUrl(String url) async {
+    final Uri uri = Uri.parse(url);
+    try {
+      await launchUrl(
+        uri,
+        mode: LaunchMode.externalApplication,
+      );
+    } catch (e1) {
+      try {
+        await launchUrl(
+          uri,
+          mode: LaunchMode.platformDefault,
+        );
+      } catch (e2) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('No app found for $url! Install an email client.')),
+        );
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+        ),
+      )
+      ..loadFile('file:///android_asset/flutter_assets/assets/www/index.html');
+  }
+
+  Future<bool> _onWillPop() async {
+    if (await _controller.canGoBack()) {
+      _controller.goBack();
+      return false; // Don't exit app
+    }
+    return true; // Exit app if no history
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: SafeArea(
+          child: WebViewWidget(controller: _controller),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () => _launchUrl('tel:+14696741200'),
+              child: Icon(Icons.phone),
+              tooltip: 'Call Us',
+            ),
+            SizedBox(height: 10),
+            FloatingActionButton(
+              onPressed: () => _launchUrl('mailto:info@cdconstructor.com?subject=Contact%20Us'),
+              child: Icon(Icons.email),
+              tooltip: 'Email Us',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
